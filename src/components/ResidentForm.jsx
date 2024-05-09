4d7c16654be41734599d9191d002949786be7001
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-05-09 14:56:38 +0500

    g

--- src/components/ResidentForm.jsx
+++ src/components/ResidentForm.jsx
@@ -26,13 +26,6 @@ const ResidentForm = () => {
       const response = await axios.post(
         "https://directory-management.onrender.com/api/v1/resident/add",
         { FullName, Email, Phone, HouseNumber, CNIC, vehicles, relatives }
-        // {
-        //   headers: {
-        //     "Access-Control-Allow-Origin":
-        //       "https://directory--sigma.vercel.app",
-        //     // Add other custom headers if needed
-        //   },
-        // }
       );
       if (response.data.success) {
         console.log(response.data);
