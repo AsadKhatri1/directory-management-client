3e18b2ed74a0ab5017332571e0248e5affa61a25
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-07-18 16:36:22 +0500

    filters and incomes

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -438,7 +438,15 @@ const ExpenseForm = () => {
             </div>
           </div>
         </div>
-        <div className="mt-3">
+        <div
+          className="mt-3 p-3"
+          style={{
+            backgroundColor: "#263043",
+            borderRadius: "12px",
+            boxShadow:
+              "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px",
+          }}
+        >
           <h5>FILTERS</h5>
           <div className="row">
             <div className="col-md-6">
